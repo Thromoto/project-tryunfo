2c2dc52f64062ecf6803a95d16b55418b3955273
Thiago Omoto <thi.omoto@gmail.com>
2022-09-28 19:59:41 -0300

    requisito 5 done

--- src/App.js
+++ src/App.js
@@ -15,8 +15,32 @@ class App extends React.Component {
       cardImage: '',
       cardRare: '',
       cardTrunfo: '',
+      isSaveButtonDisabled: true,
     };
   }
+
+  enableSaveBtn = () => {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const { cardImage, cardRare } = this.state;
+
+    const name = cardName.length > 0;
+    const descricao = cardDescription.length > 0;
+    const img = cardImage.length > 0;
+    const raro = cardRare.length > 0;
+    const inputPalavras = name && descricao && img && raro;
+
+    const minimo = 0;
+    const maximo = 90;
+    const soma = 210;
+
+    const at1 = +cardAttr1 >= minimo && +cardAttr1 <= maximo;
+    const at2 = +cardAttr2 >= minimo && +cardAttr2 <= maximo;
+    const at3 = +cardAttr3 >= minimo && +cardAttr3 <= maximo;
+    const atTotal = ((+cardAttr1 + +cardAttr2 + +cardAttr3) <= soma);
+    const atributos = at1 && at2 && at3 && atTotal;
+
+    this.setState({ isSaveButtonDisabled: !(inputPalavras && atributos) });
+  };
 
   onInputChange = ({ target }) => {
     const { name } = target;
@@ -24,12 +48,12 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
-    });
+    }, () => this.enableSaveBtn());
   };
 
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3 } = this.state;
-    const { cardImage, cardRare, cardTrunfo } = this.state;
+    const { cardImage, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
 
     return (
       <div>
@@ -43,6 +67,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
         />
         <Card
